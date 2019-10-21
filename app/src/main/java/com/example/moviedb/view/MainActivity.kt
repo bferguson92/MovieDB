99e23a2d762a1fe2c26af1ced282aa2e4d70eb12
brianferguson <brian.ferguson@mobileappscompany.us>
2019-10-21 17:47:10 -0400

    added pojo objects from api

--- app/src/main/java/com/example/moviedb/view/MainActivity.kt
+++ app/src/main/java/com/example/moviedb/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.moviedb
+package com.example.moviedb.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.moviedb.R
 
 class MainActivity : AppCompatActivity() {
 
